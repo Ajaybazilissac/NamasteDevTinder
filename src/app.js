aff1e3f3bc5b4e0a7ca549fc526c6be3fad85461
Ajay Bazil Issac <ajaybazilissac93@gmail.com>
2025-08-24 19:43:18 +0530

    multiple- routeHandlers

--- src/app.js
+++ src/app.js
@@ -2,12 +2,29 @@ const express = require('express');
 const app = express();
 
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params)
-    res.send({firstName:"Ajay", lastName:"Issac"})
-})
-
-
+app.use("/user",(req,res,next) => {
+    //Rout handler
+    console.log("Rout Handler 1");
+   // res.send("Rout handler 1");
+   next()
+},(req,res,next) => {
+    //Rout handler
+     console.log("Rout Handler 2");
+    //res.send("Rout handler 2")
+     next()
+     },
+   
+(req,res,next) => {
+    //Rout handler
+     console.log("Rout Handler 3");
+   // res.send("Rout handler 3")
+    next()
+   },
+   
+(req,res) => {
+    //Rout handler
+     console.log("Rout Handler 4");
+    res.send("Rout handler 4")});
 
 app.listen(7777,() => {
     console.log('Server is running on port 7777');
